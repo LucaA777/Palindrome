5f25f9967af47d968ed2b57c37d2f04d9d7464dd
Luca <luca@Federicos-Mac-Studio-Ultra.local>
2025-09-02 22:34:39 -0700

    Progress

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -3,21 +3,41 @@
 
 using namespace std;
 
+
+void stripCharArrayToAlphaNumeric(char s[80]);
+void charArrayToLowerCase(char s[80]);
+
 int main() {
 
   char str[80];
 
   cin.get(str, 80);
   cin.get();
-
-  int ASCII = int(str[0]);
-  cout << str << ":" << ASCII << endl;
+  
+  stripCharArrayToAlphaNumeric(str);
+  charArrayToLowerCase(str);
+  cout << str << endl;
 
   
   return (0);
 }
 
-void stripCharArrayToAlphaNumeric(char[] str) {
+void charArrayToLowerCase(char s[80]) {
+  for (int i = 0; i < 80; i++) {
+    /*
+      Looks at all ASCII values, any between 65 and 90 are upper case.
+      Adding 32 to them will convert them to lower case
+     */
+
+    int cASCII = int(s[i]);
+    if (cASCII >= 65 && cASCII <= 90) {
+      s[i] = char(cASCII + 32);
+    }
+  }
+}
+
+
+void stripCharArrayToAlphaNumeric(char s[80]) {
   for (int i = 0; i < 80; i++) {
     /*
       Cuts chars with ASCII values not in the ranges:
@@ -26,15 +46,14 @@ void stripCharArrayToAlphaNumeric(char[] str) {
       - 97-122
      */
 
-    int cASCII = int(str[i]);
+    int cASCII = int(s[i]);
     if ( !(cASCII >= 48 && cASCII <= 57) &&
 	 !(cASCII >= 65 && cASCII <= 90) &&
 	 !(cASCII >= 97 && cASCII <= 122)) {
 
-      
+      memmove(&s[i], &s[i + 1], 80 - i - 1);
     }
   }
+
+  
 }
-
-
-
